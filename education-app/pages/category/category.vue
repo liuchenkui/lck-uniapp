3cb03188ecd60e733de7fff1128b7b2febfc5a9a
liuchenkui <2848709600@qq.com>
2022-09-20 21:49:42 +0800

    education

---
+++ education-app/pages/category/category.vue
@@ -0,0 +1,127 @@
+<template>
+	<view>
+		<view class="cate-box">
+			<scroll-view scroll-y="true" class="left" :show-scrollbar="false">
+				<view>
+					<view class="left-item active">
+						Java
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+					<view class="left-item">
+						前端
+					</view>
+				</view>
+			</scroll-view>
+
+
+			<view class="right">
+				<view class="right-box">
+					<view class="right-item">
+						noasfo
+					</view>
+					<view class="right-item">
+						noasfo
+					</view>
+					<view class="right-item">
+						noasfo
+					</view>
+					<view class="right-item">
+						noasfo
+					</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.active {
+		color: #345dc2 !important;
+	}
+
+	.active::before {
+		content: "";
+		position: absolute;
+		width: 6rpx;
+		height: 50rpx;
+		background-color: #345dc2;
+		left: 0;
+		top: 35%;
+	}
+
+	.cate-box {
+		width: 100%;
+		display: flex;
+
+		.left {
+			width: 26%;
+			text-align: center;
+			height: calc(100vh - 100rpx);
+			background-color: #f8f9fb;
+
+			.left-item {
+				height: 140rpx;
+				line-height: 140rpx;
+				font-size: 30rpx;
+				font-weight: 400;
+				position: relative;
+				color: #8c8c8c;
+			}
+		}
+
+		.right {
+			flex: 1;
+
+			.right-box {
+				width: 100%;
+				padding: 5%;
+
+				.right-item {
+					display: inline-block;
+					padding: 2px 8px;
+					box-sizing: border-box;
+					border-radius: 30rpx;
+					border: 1px solid #bbb;
+					margin: 10rpx;
+					color: #33352d;
+				}
+			}
+		}
+	}
+</style>
---
+++ education-app/pages/searchView/searchView.vue
@@ -0,0 +1,90 @@
+<template>
+	<view>
+		<!-- 搜索区域 -->
+		<myInput></myInput>
+		
+		<view class="title">
+			热门搜索
+		</view>
+		
+		<view class="cate-box">
+			<view class="cate-item">
+				java
+			</view>
+			<view class="cate-item">
+				java
+			</view>
+			<view class="cate-item">
+				java
+			</view>
+			<view class="cate-item">
+				java
+			</view>
+			<view class="cate-item">
+				java
+			</view>
+			<view class="cate-item">
+				java
+			</view>
+		</view>
+		
+		<view class="history">
+			<view>
+				历史搜索
+			</view>
+			<view class="clear">
+				清空
+			</view>
+		</view>
+		<view class="bottom">
+			<view class="cate-item">
+				java
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.history{
+		display: flex;
+		justify-content: space-between;
+		.clear{
+			color: #bbb;
+		}
+	}
+.title,.history,.bottom{
+	width: 100%;
+	padding: 2% 3%;
+	box-sizing: border-box;
+	font-size: 35rpx
+}
+.cate-box{
+	width: 100%;
+	display: flex;
+	justify-content: flex-start;
+	flex-wrap: wrap;
+	padding: 3%;
+	box-sizing: border-box;
+
+}
+	.cate-item{
+		display: inline-block;
+		margin: 5px;
+		padding: 3px 12px;
+		font-size: 10px;
+		text-align: center;
+		border: 1px solid #aaa;
+		color: #aaa;
+		border-radius: 10rpx;
+	}
+</style>
